d4bda1cd63704d0313d735a5e61061bd0a225bdf
hzpcactus <1416860873@qq.com>
2020-06-06 11:02:55 +0800

    feat:修复数据库一段时间后自动断开问题
    feat:修复数据库一段时间后自动断开问题

--- routes/friends.js
+++ routes/friends.js
@@ -4,16 +4,38 @@ const router = express.Router();
 const socket = require('../tools/socket1');
 const mysql = require('mysql');
 const multiparty = require("multiparty")
-const connection = mysql.createConnection({     
+const db_config = {     
   host     : 'localhost',       
   user     : 'root',              
   password : '19980605',       
   port: '3306',                   
   database: 'campus-social' ,
   timezone: "08:00"
-}); 
- 
-connection.connect();
+};
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('又断开了：', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      console.log('db error执行重连:' + err.message);
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+handleDisconnect();
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
